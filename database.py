85ecfb174f1a252249c4a9b65c51bdd90df6d2df
Connor Penny <cgp19@duke.edu>
2020-10-14 20:55:20 -0400

    Connection to and test of database on VM

--- database.py
+++ database.py
@@ -1,10 +1,19 @@
 # database
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test:password@152.3.52.135/test1'
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
 
+class test1(db.Model):
+    __tablename__ = 'test1'
+    id = db.Column('a', db.Integer(), primary_key = True)
 
 
 
@@ -14,4 +23,5 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    print(db.session.query(test1).all())
