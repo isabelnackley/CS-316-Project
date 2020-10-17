9546ee4aaa298df6faf965b4eb1c93af7919f109
Connor Penny <cgp19@duke.edu>
2020-10-17 16:34:35 -0400

    Copied function skeletons from sakai app

--- database.py
+++ database.py
@@ -11,28 +11,122 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
+User = (2,1) #dummy user who is placeholder until we make login
 
-
-@app.route('/main/')
+@app.route('/')
 def mainPage():
     result = db.session.query(relations.Item.sku, relations.Item.title, relations.Item.category, relations.Item.price, relations.Item.rating).all()
     return result #TODO: send this to GUI for display
 
 @app.route('/item/<sku>')
 def itemPage(sku):
-    result = db.session.query(relations.Item.sku, relations.Item.title, relations.Item.category, relations.Item.price, relations.Item.rating, relations.Item.description).filter(relations.Item.sku == sku)
+    result = db.session.query(relations.Item.sku, relations.Item.title, relations.Item.category, relations.Item.price,
+                              relations.Item.rating, relations.Item.description).filter(relations.Item.sku == sku)
     return result #TODO: send this to GUI for display
 
-@app.route('/item/<sku>/purchase')
-def
+@app.route('/results', methods=['GET', 'POST'])
+def search_results(search):
+    result = db.session.query(relations.Item.sku, relations.Item.title, relations.Item.category, relations.Item.price,
+                              relations.Item.rating, relations.Item.description).filter(relations.Item.title == search)
+    #return render_template('result.html', placetaker =placetaker) TODO: send this to GUI for display
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route("/add")
+def admin():
+    placetaker = ''
+    return render_template('add.html', placetaker =placetaker)
+
+@app.route("/addItem", methods=["GET", "POST"])
+def addItem():
+    print(msg)
+    return redirect(url_for('root'))
+
+@app.route("/remove")
+def remove():
+    data = ''
+    return render_template('remove.html', data=data)
+
+@app.route("/removeItem")
+def removeItem():
+    print(msg)
+    return redirect(url_for('root'))
+
+@app.route("/displayCategory")
+def displayCategory():
+    placetaker = ''
+    return render_template('displayCategory.html', placetaker =placetaker)
+
+@app.route("/account/profile")
+def profileHome():
+    placetaker = ''
+    return render_template("profileHome.html", placetaker =placetaker)
+
+@app.route("/account/profile/edit")
+def editProfile():
+    placetaker = ''
+    return render_template("editProfile.html", placetaker =placetaker)
+
+@app.route("/account/profile/changePassword", methods=["GET", "POST"])
+def changePassword():
+    placetaker = ''
+    return render_template("changePassword.html", placetaker =placetaker)
+
+@app.route("/updateProfile", methods=["GET", "POST"])
+def updateProfile():
+    return redirect(url_for('editProfile'))
+
+@app.route("/loginForm")
+def loginForm():
+    return render_template('login.html', error='')
+
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    if True:
+
+        return redirect(url_for('root'))
+    else:
+        error = 'Invalid UserId / Password'
+        return render_template('login.html', error=error)
+
+@app.route("/productDescription", methods=['GET', 'POST'])
+def productDescription():
+    placetaker = ''
+    return render_template("productDescription.html", placetaker =placetaker)
+
+@app.route("/addToCart")
+def addToCart():
+    print('')
+    return redirect(url_for('root'))
+
+@app.route("/cart", methods=['GET', 'POST'])
+def cart():
+    placetaker = ''
+    return render_template("cart.html", placetaker =placetaker)
+
+@app.route("/removeFromCart")
+def removeFromCart():
+
+    return redirect(url_for('root'))
+
+@app.route("/logout")
+def logout():
+
+    return redirect(url_for('root'))
+
+def is_valid(email, password):
+    if True:
+        return True
+    return False
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    msg = ''
+    return render_template("login.html", error=msg)
+
+@app.route("/registerationForm")
+def registrationForm():
+    return render_template("register.html")
 
 if __name__ == '__main__':
     #app.run()
-    print(db.session.query(relations.User.id, relations.User.email).all())
-    db.session.execute('')
+    print(db.session.query(relations.User.id, relations.User.is_buyer).all())
 
